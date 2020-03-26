53c065ad0cceecc1e12010b54d25042601423c98
Eka <eka_solnishko@mail.ru>
2020-03-26 20:58:19 +0600

    Commit

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -2,9 +2,10 @@ package com.company;
 
 public class BankAccount {
 
-    public static final String ANSI_GREEN = "\u001B[36m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String GREEN = "\u001B[36m";
+    public static final String RED = "\u001B[31m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
 
     private double amount;
 
@@ -15,17 +16,19 @@ public class BankAccount {
     public void deposit(double sum) {
         if (sum > 0) {
             amount = amount + sum;
-            System.out.println(ANSI_YELLOW + "Деньги на счете: " + ANSI_GREEN + amount);
-            System.out.println(ANSI_YELLOW + "Максимальный лимит снятия денег: " + ANSI_GREEN + "6000");
+            System.out.println(YELLOW + "Деньги на счете: " + GREEN + amount);
+            System.out.println(YELLOW + "Максимальный лимит снятия денег: " + GREEN + "6000");
         }
     }
 
     public void withDraw(double sum) throws LimitException {
         if (sum > amount) {
-            throw new LimitException(ANSI_RED + "Недостаточно средств ", amount);
+            throw new LimitException(RED + "Недостаточно средств для снятия максимального лимита ", amount);
         }
         amount = amount - sum;
-        System.out.println(getAmount());
+        System.out.println(YELLOW +"Снимаем "+ GREEN + sum);
+        System.out.println(YELLOW +"Остаток ");
+        System.out.println(GREEN + getAmount());
 
     }
 
